f605ad715ae2b2ca82e0b02999335a673afb5388
Elliott Payne <e.payne000@gmail.com>
2020-09-23 22:56:22 -0500

    moved choiceArray into function so it resets everytime, rearanged and cleaned up code.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Arrays for lowercase, uppercase, numeric, special characters, and an empty array to hold choices.
 
-var choiceArray = [];
+
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = [0,1,2,3,4,5,6,7,8,9];
@@ -9,6 +9,7 @@ var specChar = ["`","_","@",">","=","<",";","/",".",",","'","%","$","#","+", "-"
 // My function to generate a random password between 8 and 128 characters long.
 function generatePassword() {
 
+  var choiceArray = [];
   var passwordRandom = "";
 
   //
@@ -27,60 +28,41 @@ function generatePassword() {
   var passucase = confirm("Would you like to use uppercase letters?")
   var passnum = confirm("Would you like to use numbers in your password?")
   var passSpec = confirm("Would you like to use special characters?")
-  if(passlcase === false && passucase === false && passnum === false && passSpec === false){
+
+  //
+  if(passlcase){
+    choiceArray.push(lower);
+  }
+  if(passucase){
+    choiceArray.push(upper);
+  }
+  if(passnum){
+    choiceArray.push(numeric);
+  }
+  if(passSpec){
+    choiceArray.push(specChar);
+  }
+  
+  if(!passlcase && !passucase && !passnum && !passSpec){
     alert("Must select at least one character type option.")
     return
   }
   
-  //
-  if(passlcase===true){
-    choiceArray.push(lower);
-  }
-  if(passucase===true){
-    choiceArray.push(upper);
-  }
-  if(passnum===true){
-    choiceArray.push(numeric);
-  }
-  if(passSpec===true){
-    choiceArray.push(specChar);
-  }
-
   //Loop for the password generator based on length user entered.
   for(var i=0;i<passLength;i++){
 
-    var 
+    var randomArray = Math.floor(Math.random()*choiceArray.length);
+    var randomArrayChoice = choiceArray[randomArray];
+    var randomNum = Math.floor(Math.random()*randomArrayChoice.length);
+    var randomCharacter = randomArrayChoice[randomNum];
+
+    passwordRandom += randomCharacter;
   }
 
+  password.textContent = passwordRandom;
+
 }
 
-generatePassword()
-
-console.log (choiceArray);
-
-var passOptions = {
-  length:passLength, 
-  passlcase,
-  passucase,
-  passnum,
-  passSpec
-}
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-//lowercase, uppercase, numeric, and/or special characters
+var generate = document.getElementById("generate");
+var password = document.getElementById("password");
+generate.onclick = generatePassword;
