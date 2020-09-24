0e56c7c3cbbf75deabe25bbe207b29eca99f4aa9
Elliott Payne <e.payne000@gmail.com>
2020-09-24 15:35:40 -0500

    finished commenting and the README.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # HW3-PasswordGen
+
+## Purpose
+This project's purpose is to generate a strong password based on user preferences. The application validates user input and makes sure that at least one character type option is selected, as well as the pasword length is between 8 and 128 characters. After the user chooses their preferences, they will be given a password matching their preferences.
+
+Link to deplyed application: https://mcduderson.github.io/HW3-PasswordGen/
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Arrays for lowercase, uppercase, numeric, special characters, and an empty array to hold choices.
-
-
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = [0,1,2,3,4,5,6,7,8,9];
@@ -12,7 +10,7 @@ function generatePassword() {
   var choiceArray = [];
   var passwordRandom = "";
 
-  //
+  //Prompt user to choose their password length, with validation.
   var passLength = prompt("Enter desired password length.") 
   if(passLength < 8 || passLength > 128){
     alert("Password must be between 8 and 128 characters.");
@@ -23,13 +21,13 @@ function generatePassword() {
     return
   }
 
-  //
+  //gather booleans based on the user preference.
   var passlcase = confirm("Would you like to use lowercase letter?")
   var passucase = confirm("Would you like to use uppercase letters?")
   var passnum = confirm("Would you like to use numbers in your password?")
   var passSpec = confirm("Would you like to use special characters?")
 
-  //
+  //Push the selected arrays to the choiceArray, with validation.
   if(passlcase){
     choiceArray.push(lower);
   }
@@ -42,7 +40,6 @@ function generatePassword() {
   if(passSpec){
     choiceArray.push(specChar);
   }
-  
   if(!passlcase && !passucase && !passnum && !passSpec){
     alert("Must select at least one character type option.")
     return
@@ -63,6 +60,7 @@ function generatePassword() {
 
 }
 
+//Button interactions.
 var generate = document.getElementById("generate");
 var password = document.getElementById("password");
 generate.onclick = generatePassword;
